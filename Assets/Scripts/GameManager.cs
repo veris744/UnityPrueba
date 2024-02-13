77e5456c4284270adf5d08b9e11dfb43cbd779d2
veris744 <71214562+veris744@users.noreply.github.com>
2024-02-13 10:00:28 +0100

    start

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance
+    {
+        get;
+        private set;
+    }
+
+    public void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
